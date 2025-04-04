3444865307a46a06d37084d4e95794e91c273b71
Sasith Methmal <52422394+sasithmj@users.noreply.github.com>
2025-04-04 19:36:47 +0530

    one

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,4 +1,4 @@
-const UserService = require("../services/UserService");
+const UserService = require("../Services/UserService");
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
