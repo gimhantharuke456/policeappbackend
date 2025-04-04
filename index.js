f4e6310b82098c7ba64290709b29aabd04d79ca2
Sasith Methmal <52422394+sasithmj@users.noreply.github.com>
2025-04-04 18:10:11 +0530

    vercel config4

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const db = require("./Config/DBconfig");
 const express = require("express");
 const path = require("path");
 
-const port = 3001;
+const port = process.env.PORT || 3002;
 // const SchedulePickupModel=require("./models/SchedulePickupModel")
 GOOGLE_APPLICATION_CREDENTIALS =
   "C:UserssasitDesktopProject 02Backendgoogle.json";
-
+ 
 app.get("/", (req, res) => {
   res.send("Hello world !!!");
 });
 
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.listen(port, "0.0.0.0", () => {
+// app.listen(port, "0.0.0.0", () => {
+//   console.log(`Server is runnnig on port ${port}`);
+// });
+app.listen(port, () => {
   console.log(`Server is runnnig on port ${port}`);
 });
-
